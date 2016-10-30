508e017905ba68da82dcb1a41549954c6bcd2233
jwdean <jwdean755@gmail.com>
2016-10-30 13:22:36 -0700

    Created more system tests.  All Passed.  Began Documentation

--- ExampleCCodeTutorial/systemTests/STExampleCCode.py
+++ ExampleCCodeTutorial/systemTests/STExampleCCode.py
@@ -41,11 +41,71 @@ def IVManyRandomCharsAsAge():
         return 1
 
 
+def IVLowerMenu0():
+    proc = RunExecutable(r'../build/ExampleCCodeTutorial')
+    exeOut,exeErr = proc.communicate('3\n0\n1\n1\n1\n')
+    print exeOut
+    if 'error' in exeOut:
+        print 'IVLowerMenu0 SUCCESS'
+        return 0
+    else:
+        print 'IVLowerMenu0 FAILURE'
+        return 1
+
+def IVUpperMenu6():
+    proc = RunExecutable(r'../build/ExampleCCodeTutorial')
+    exeOut,exeErr = proc.communicate('3\n6\n1\n1\n1\n')
+    print exeOut
+    if 'error' in exeOut:
+        print 'IVUpperMenu6 SUCCESS'
+        return 0
+    else:
+        print 'IVUpperMenu6 FAILURE'
+        return 1
+
+def VLowerMenu1():
+    proc = RunExecutable(r'../build/ExampleCCodeTutorial')
+    exeOut,exeErr = proc.communicate('3\n1\n1\n1\n')
+    print exeOut
+    if 'error' not in exeOut:
+        print 'VLowerMenu1 SUCCESS'
+        return 0
+    else:
+        print 'VLowerMenu1 FAILURE'
+        return 1
+
+def VUpperMenu5():
+    proc = RunExecutable(r'../build/ExampleCCodeTutorial')
+    exeOut,exeErr = proc.communicate('3\n5\n1\n1\n1\n')
+    print exeOut
+    if 'error' not in exeOut:
+        print 'VUpperMenu5 SUCCESS'
+        return 0
+    else:
+        print 'VUpperMenu5 FAILURE'
+        return 1
+
 def main():
     result = IVLetterAsAge()
     result += IVManyLettersAsAge()
     result += IVManyRandomCharsAsAge()
-    #result += 
+    result += IVLowerMenu0()
+    result += IVUpperMenu6()
+    result += VLowerMenu1()
+    result += VUpperMenu5()
+    #result += VInputAge2()
+    #result += VInputAge13()
+    #result += VInputAge12()
+    #result += VInputAge22()
+    #result += VInputAge23()
+    #result += VInputAge4And3Candy()
+    #result += VInputAge5And5Candy()
+    #result += VInputAge10And10Candy()
+    #result += VInputAge10And9Candy()
+    if 0 == result:
+        print '\nAll System Tests PASSED\n'
+    else:
+        print '\n' + result + ' System Tests FAILED\n'
 
 if __name__ == "__main__":
     main()
